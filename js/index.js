10d4d2903ebcdcf0acb36a4e3abb7ab0d53634dd
Nikolay Trunov <anteycu@gmail.com>
2023-09-18 17:51:40 +0300

    added typescript

--- js/index.js
+++ js/index.js
@@ -1,32 +1,32 @@
-const fetchButton = document.querySelector(".js-btn");
-const numberInput = document.querySelector(".js-input");
-const outputBlock = document.querySelector(".js-rootOutput");
+// const fetchButton = document.querySelector(".js-btn");
+// const numberInput = document.querySelector(".js-input");
+// const outputBlock = document.querySelector(".js-rootOutput");
 
-fetchButton.addEventListener("click", () => {
-  if (numberInput.value) {
-    fetchPokemon(numberInput.value).then((data) => createMarkup(data));
-  } else {
-    console.log("Nothing entered");
-  }
-});
+// fetchButton.addEventListener("click", () => {
+//   if (numberInput.value) {
+//     fetchPokemon(numberInput.value).then((data) => createMarkup(data));
+//   } else {
+//     console.log("Nothing entered");
+//   }
+// });
 
-function fetchPokemon(id) {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`).then((response) =>
-    response.json()
-  );
-}
+// function fetchPokemon(id) {
+//   return fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`).then((response) =>
+//     response.json()
+//   );
+// }
 
-function createMarkup(data) {
-  const { name, height, weight, stats } = data;
-  const mapedStats = stats.map(
-    ({ base_stat, stat: { name } }) => `<li>Stat: ${name} = ${base_stat}</li>`
-  );
-  outputBlock.innerHTML = `<ul>Name of this pokemon: ${name}
-    <li>Height: ${height}</li>
-    <li>Weight: ${weight}</li>
-    <li><ul>Pokemon stats: ${mapedStats.join("")}</ul></li>
-    </ul>`;
-}
+// function createMarkup(data) {
+//   const { name, height, weight, stats } = data;
+//   const mapedStats = stats.map(
+//     ({ base_stat, stat: { name } }) => `<li>Stat: ${name} = ${base_stat}</li>`
+//   );
+//   outputBlock.innerHTML = `<ul>Name of this pokemon: ${name}
+//     <li>Height: ${height}</li>
+//     <li>Weight: ${weight}</li>
+//     <li><ul>Pokemon stats: ${mapedStats.join("")}</ul></li>
+//     </ul>`;
+// }
 
 // numberInput.addEventListener("change", (e) =>
 //   getUserNumber(e.currentTarget.value)
